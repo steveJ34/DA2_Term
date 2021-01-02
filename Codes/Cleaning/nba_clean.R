452f04b71aac17a4ca42e3cd9a0871f3e33a40bf
Steve Janco <70906188+steveJ34@users.noreply.github.com>
2021-01-02 21:01:36 +0100

    update code

--- Codes/Cleaning/nba_clean.R
+++ Codes/Cleaning/nba_clean.R
@@ -100,5 +100,5 @@ df_round_filter <- df_full %>% filter( draft_round > 2 )
 df_full <- df_full %>% filter( draft_round < 3 )
 
 # Saving the clean data 
-write.csv(df_full, "/Users/steve_j/Documents/CEU /data_analysis/DA_2/DA2_Term//nba_clean13.csv", row.names = FALSE)
+#write.csv(df_full, "/your path//nba_clean13.csv", row.names = FALSE)
 
